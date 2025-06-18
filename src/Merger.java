de94822c053f40ba7cb67952eaa24016f6ac6465
shiva.avula <shiva.avula@medplusindia.com>
2025-06-18 12:47:50 +0530

    new change

--- src/Merger.java
+++ src/Merger.java
@@ -4,6 +4,8 @@ public class   Merger {
 	public class Meger {
         public static void main(String[] args) {
             System.out.println("Hello, this is a merge Branch!");
+            System.out.println("Hello, this is a merge Branch!");
+            System.out.println("Hello, this is a merge Branch!");
         }
     }
 }
