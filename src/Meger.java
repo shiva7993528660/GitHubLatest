7b1f1715940e463aafe37edd5eebcfec4d485f54
shiva.avula <shiva.avula@medplusindia.com>
2025-06-18 12:08:42 +0530

    commit

--- src/Meger.java
+++ src/Meger.java
@@ -1,8 +1,7 @@
 module Merger {
 	public class Meger {
         public static void main(String[] args) {
-            System.out.println("Hello, this is a merge example!");
-             System.out.println("Hello, this is a merge example!");
+           System.out.println("Hello from MERGE!");
         }
     }
 }
