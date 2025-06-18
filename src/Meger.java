950c84c6adfa6ee1d65b41b674e4e01380d4f78d
shiva.avula <shiva.avula@medplusindia.com>
2025-06-18 12:01:03 +0530

    meger

--- src/Meger.java
+++ src/Meger.java
@@ -2,6 +2,7 @@ module Merger {
 	public class Meger {
         public static void main(String[] args) {
             System.out.println("Hello, this is a merge example!");
+             System.out.println("Hello, this is a merge example!");
         }
     }
 }
